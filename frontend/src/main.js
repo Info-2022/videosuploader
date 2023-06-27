72360497c888a764d13a68e56a90c507b4971ba4
Info-2022 <124354756+Info-2022@users.noreply.github.com>
2023-06-27 15:45:21 +0530

    Add files via upload

---
+++ frontend/src/main.js
@@ -0,0 +1,86 @@
+let isConfigUpdate = false;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function uploadToS3Bucket(stream, credential, cd) {
+
+    try {
+        if (!window.AWS) {
+            return
+        }
+        if (!isConfigUpdate) {
+            window.AWS.config.update(({ region: credential.region }));
+            isConfigUpdate = true;
+        }
+
+        let s3 = new window.AWS.S3({
+            credentials: new window.AWS.Credentials({
+                apiVersion: 'latest',
+                accessKeyId: credential.accressKeyId,
+                secretAccessKey: credential.secretAccessKey,
+                signatureVersion: credential.signatureVersion,
+                region: credential.region,
+                Bucket: credential.Bucket
+            })
+        });
+
+        let uploadItem = await s3.upload({
+            Bucket: credential.Bucket,
+            Key: getRndInteger(100000, 999999) + "",// name for the bucket file
+            ContentType: document.getElementById("fileToUpload").files[0].type,
+            Body: stream
+        }).on("httpUploadProgress", function (progress) {
+            console.log("progress=>", progress)
+            cd(getUploadingProgress(progress.loaded, progress.total));
+        }).promise();
+        console.log("uploadItem=>", uploadItem)
+        alert(`${uploadItem.key} Uploaded successfully to AWS`);
+        return uploadItem;
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
+function getUploadingProgress(uploadSize, totalSize) {
+    let uploadProgress = (uploadSize / totalSize) * 100;
+    return Number(uploadProgress.toFixed(0));
+}
+
+export default async function uploadMedia() {
+    try {
+        let credentialRequest = {
+            accressKeyId: 'AKIA25CVQLUX3V7T7LIO',
+            secretAccessKey: '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',
+            signatureVersion: 'v4',
+            region: 'ap-south-1',
+            Bucket: 'mp4-avi-uploader'
+        };
+        let mediaStreamRequest = getFile(document.getElementById("fileToUpload").files[0])
+        const [mediaStream] = await Promise.all([
+            mediaStreamRequest
+        ])
+        await uploadToS3Bucket(mediaStream, credentialRequest, (progress) => {
+            console.log(progress)
+        })
+    }
+    catch {
+        alert('Please select file to upload');
+    }
+}
+
+async function getFile(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            resolve(e.target.result);
+        };
+        reader.onerror = (err) => {
+            reject(false);
+        };
+        reader.readAsArrayBuffer(file);
+    });
+};
